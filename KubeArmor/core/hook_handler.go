0a3a8b0caef579a7fad8e28744281c134ed944bd
AbdelrahmanElawady <abdoelawady125@gmail.com>
2024-04-21 21:52:34 +0200

    Detach a process to pass all containers to KubeArmor
    Signed-off-by: AbdelrahmanElawady <abdoelawady125@gmail.com>

--- KubeArmor/core/hook_handler.go
+++ KubeArmor/core/hook_handler.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+	"sync/atomic"
 
 	kl "github.com/kubearmor/KubeArmor/KubeArmor/common"
 	cfg "github.com/kubearmor/KubeArmor/KubeArmor/config"
@@ -18,6 +19,8 @@ import (
 
 const kubearmorDir = "/var/run/kubearmor"
 
+// ListenToHook starts listening on a UNIX socket and waits for container hooks
+// to pass new containers
 func (dm *KubeArmorDaemon) ListenToHook() {
 	if err := os.MkdirAll(kubearmorDir, 0750); err != nil {
 		log.Fatal(err)
@@ -32,6 +35,7 @@ func (dm *KubeArmorDaemon) ListenToHook() {
 
 	defer socket.Close()
 	defer os.Remove(listenPath)
+	ready := &atomic.Bool{}
 
 	for {
 		conn, err := socket.Accept()
@@ -39,12 +43,17 @@ func (dm *KubeArmorDaemon) ListenToHook() {
 			log.Fatal(err)
 		}
 
-		go dm.handleConn(conn)
+		go dm.handleConn(conn, ready)
 	}
 
 }
 
-func (dm *KubeArmorDaemon) handleConn(conn net.Conn) {
+// handleConn gets container details from container hooks.
+func (dm *KubeArmorDaemon) handleConn(conn net.Conn, ready *atomic.Bool) {
+	// We need to makes sure that no containers accepted until all containers created before KubeArmor
+	// are sent first. This is done mainly to avoid race conditions between hooks sending in
+	// data that some containers were deleted only for process responsible for sending previous containers
+	// to send that these containers are created. Which will leave KubeArmor in an incorrect state.
 	defer conn.Close()
 	buf := make([]byte, 4096)
 
@@ -59,6 +68,7 @@ func (dm *KubeArmorDaemon) handleConn(conn net.Conn) {
 
 		data := struct {
 			Operation string          `json:"operation"`
+			Detached  bool            `json:"detached"`
 			Container types.Container `json:"container"`
 		}{}
 
@@ -66,7 +76,30 @@ func (dm *KubeArmorDaemon) handleConn(conn net.Conn) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		dm.Logger.Printf("got container %s", data.Container.ContainerID)
+
+		if data.Detached {
+			// we want KubeArmor to start accepting containers after
+			// all previous container are set
+			defer ready.Store(true)
+		} else if !ready.Load() {
+			_, err = conn.Write([]byte("err"))
+			if err == io.EOF {
+				return
+			} else if err != nil {
+				log.Println(err)
+				return
+			}
+			continue
+		}
+
+		_, err = conn.Write([]byte("ok"))
+		if err == io.EOF {
+			return
+		} else if err != nil {
+			log.Println(err)
+			return
+		}
+
 		if data.Operation == "create" {
 			dm.handleContainerCreate(data.Container)
 		} else {
@@ -75,18 +108,59 @@ func (dm *KubeArmorDaemon) handleConn(conn net.Conn) {
 	}
 }
 func (dm *KubeArmorDaemon) handleContainerCreate(container types.Container) {
+	endpoint := types.EndPoint{}
+
+	dm.Logger.Printf("added %s", container.ContainerID)
 
 	dm.ContainersLock.Lock()
+	if _, ok := dm.Containers[container.ContainerID]; !ok {
+		dm.Containers[container.ContainerID] = container
+		dm.ContainersLock.Unlock()
+	} else if dm.Containers[container.ContainerID].PidNS == 0 && dm.Containers[container.ContainerID].MntNS == 0 {
+		c := dm.Containers[container.ContainerID]
+		c.MntNS = container.MntNS
+		c.PidNS = container.PidNS
+		c.AppArmorProfile = container.AppArmorProfile
+		dm.Containers[c.ContainerID] = c
+		dm.ContainersLock.Unlock()
+
+		dm.EndPointsLock.Lock()
+		for idx, endPoint := range dm.EndPoints {
+			if endPoint.NamespaceName == container.NamespaceName && endPoint.EndPointName == container.EndPointName && kl.ContainsElement(endPoint.Containers, container.ContainerID) {
+
+				// update apparmor profiles
+				if !kl.ContainsElement(endPoint.AppArmorProfiles, container.AppArmorProfile) {
+					dm.EndPoints[idx].AppArmorProfiles = append(dm.EndPoints[idx].AppArmorProfiles, container.AppArmorProfile)
+				}
+
+				if container.Privileged && dm.EndPoints[idx].PrivilegedContainers != nil {
+					dm.EndPoints[idx].PrivilegedContainers[container.ContainerName] = struct{}{}
+				}
+
+				endpoint = dm.EndPoints[idx]
+
+				break
+			}
+		}
+		dm.EndPointsLock.Unlock()
+	} else {
+		dm.ContainersLock.Unlock()
+	}
 	if len(dm.OwnerInfo) > 0 {
 		container.Owner = dm.OwnerInfo[container.EndPointName]
 	}
-	dm.Containers[container.ContainerID] = container
-	dm.Logger.Printf("added %s", container.ContainerID)
-	dm.ContainersLock.Unlock()
 
 	if dm.SystemMonitor != nil && cfg.GlobalCfg.Policy {
 		dm.SystemMonitor.AddContainerIDToNsMap(container.ContainerID, container.NamespaceName, container.PidNS, container.MntNS)
 		dm.RuntimeEnforcer.RegisterContainer(container.ContainerID, container.PidNS, container.MntNS)
+
+		if len(endpoint.SecurityPolicies) > 0 { // struct can be empty or no policies registered for the endpoint yet
+			dm.Logger.UpdateSecurityPolicies("ADDED", endpoint)
+			if dm.RuntimeEnforcer != nil && endpoint.PolicyEnabled == types.KubeArmorPolicyEnabled {
+				// enforce security policies
+				dm.RuntimeEnforcer.UpdateSecurityPolicies(endpoint)
+			}
+		}
 	}
 }
 func (dm *KubeArmorDaemon) handleContainerStop(containerID string) {
